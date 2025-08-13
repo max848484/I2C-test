5968f6b73b7c1d6f9a2f476bb686009805377c7c
Maxime Champagne <mchampagne@cmaisonneuve.qc.ca>
2025-08-13 15:26:47 -0400

    Correction erreurs de syntaxe

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/********************************************************************
+ * Fichier : main.cpp
+ * Auteur  : M. Champagne
+ * Date    : 2025-08-13
+ * Description :
+ *   Programme de test pour valider la communication avec un 
+ *   périphérique I2C.
+ *   - Test de l'initialisation I2C
+ *   - Vérification de la présence de l'esclave à l'adresse spécifiée
+ *   - Clignotement de la LED intégrée
+ ********************************************************************/
+
 #include <Arduino.h>
 #include <Wire.h>
 
@@ -6,10 +18,7 @@ static const uint8_t SCL_PIN = 6;
 static const uint8_t ESCLAVE_ADDR = 0x57; //adresse 0b1010111
 static const uint32_t I2C_FREQ = 100000;
 
-void scanI2C();
-
-//Cette fonction est exécuté automatiquement à la suite d'un reset. 
-//Elle n'est plus jamais appelé par la suite.
+// ====================== SETUP ===========================
 void setup() {
   //Initialisaion de la broche LED_BUILTIN en sortie
   pinMode(LED_BUILTIN, OUTPUT);
@@ -37,15 +46,12 @@ void setup() {
   if (error == 0) {
     Serial.println("✅ Réponse détectée : le périphérique est présent !");
   }
-  else if (error == 4) {
-    Serial.println("❌ Erreur inconnue lors de la communication !");
-  }
   else {
     Serial.println("❌ Aucun périphérique ne répond à cette adresse.");
   }
 }
 
-//Cette fonction est exécutée en boucle à l'infini
+// ====================== LOOP ============================
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   delay(1000);                      // wait for a second
